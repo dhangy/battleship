b271532a2266f38d538311dbab089af6891ee519
dhangy <danniebrockbank@gmail.com>
2017-10-28 16:15:22 -0600

    fixed more minor things

--- userLogin.js
+++ userLogin.js
@@ -7,11 +7,8 @@ function buttonClick() {
 }
 
 function validateLogin(){
-    var userName = document.getElementById("uName").value;
-    var password = document.getElementById("pswd").value;
-    console.log(userName);
-    console.log(password);
-
+    var userName = document.getElementById("userName").value;
+    var password = document.getElementById("password").value
     var request = new XMLHttpRequest();
     request.onreadystatechange = alertContents;
     request.open("POST", "http://universe.tc.uvu.edu/cs2550/assignments/PasswordCheck/check.php", true);
@@ -20,12 +17,12 @@ function validateLogin(){
     console.log(credential);
     request.send(credential);
 
-
-  function alertContents() {
-    if (request.readyState === XMLHttpRequest.DONE) {
-      if (request.status === 200) {
-        alert(request.responseText);
-      } else {
+    function alertContents() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                alert(request.responseText);
+            }
+            else {
         alert('There was a problem with the request.');
       }
     }

aabcb48da07bdb99cf5a8940a0064c6b470865e9
dhangy <danniebrockbank@gmail.com>
2017-10-28 15:54:27 -0600

    added the beginnings of a userLogin system

---
+++ userLogin.js
@@ -0,0 +1,33 @@
+function buttonClick() {
+    var e = document.getElementById("loginButton");
+    e.addEventListener("click", function(e) {
+        validateLogin();
+
+    });
+}
+
+function validateLogin(){
+    var userName = document.getElementById("uName").value;
+    var password = document.getElementById("pswd").value;
+    console.log(userName);
+    console.log(password);
+
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = alertContents;
+    request.open("POST", "http://universe.tc.uvu.edu/cs2550/assignments/PasswordCheck/check.php", true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var credential = "userName=" + userName +"&password=" + password;
+    console.log(credential);
+    request.send(credential);
+
+
+  function alertContents() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        alert(request.responseText);
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
+  }
